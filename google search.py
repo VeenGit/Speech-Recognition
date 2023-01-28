07547f8ed26aba33c21f8895c6f773c6ea5861d3
Vinit Agivale <66054863+VeenGit@users.noreply.github.com>
2023-01-28 16:42:25 +0530

    Add files via upload

---
+++ google search.py
@@ -0,0 +1,30 @@
+import speech_recognition as sr
+import webbrowser as wb
+
+r1 = sr.Recognizer()
+r2 = sr.Recognizer()
+r3 = sr.Recognizer()
+
+with sr.Microphone() as source:
+
+    print('[search query: search google]')
+    print('speak now')
+    audio = r3.listen(source)
+
+    if 'search' in r1.recognize_google(audio):
+        r1 = sr.Recognizer()
+        url = 'https://www.google.com/search?q='
+        with sr.Microphone() as source:
+            print('search your query')
+            audio = r1.listen(source)
+
+            try:
+                get = r1.recognize_google(audio)
+                print(get)
+                wb.get().open_new(url+get)
+            except sr.UnknownValueError:
+                print('error')
+            except sr.RequestError as e:
+                print('failed'.format(e))
+
+
